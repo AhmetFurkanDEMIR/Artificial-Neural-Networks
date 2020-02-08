094af6afe293c86943b40c588caa277ddce4e2ec
AhmetFurkanDEMIR <54184905+AhmetFurkanDEMIR@users.noreply.github.com>
2020-02-08 11:42:08 +0300

    Update ysa1.py

--- ysa1.py
+++ ysa1.py
@@ -60,21 +60,3 @@ if __name__ == "__main__":
     print(ysa.besle(array([0, 1])))
    
 # ------------- son -------------
-
-"""  Yapay Sinir Ağları örnek 2 """
-
-
-egitim_girisler = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-egitim_cikislar = array([[0, 1, 1, 0]]).T
-
-random.seed(1)
-
-agirliklar = 2 * random.random((3, 1)) - 1
-
-for iterasyon in range(10000):
-    cikis = 1 / (1 + exp(-(dot(egitim_girisler, agirliklar))))
-    agirliklar += dot(egitim_girisler.T, (egitim_cikislar - cikis) * cikis * (1 - cikis))
-
-print(1 / (1 + exp(-(dot(array([1, 0, 0]), agirliklar)))))
-
-# ------------- son -------------
